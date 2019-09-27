eae5db9764a3f0acc4dbd056d40ac4d2e25be48f
Luiz Evangelista <luiz.evangelista@b2wdigital.com>
2019-09-27 16:50:23 -0300

    ajuste controller

--- src/main/java/com/devlhse/headerstrategy/controller/CarController.java
+++ src/main/java/com/devlhse/headerstrategy/controller/CarController.java
@@ -5,7 +5,7 @@ import com.devlhse.headerstrategy.model.CarResponse;
 import com.devlhse.headerstrategy.service.CarRouterService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,15 +14,15 @@ public class CarController {
 
     private static final Logger LOG = LoggerFactory.getLogger(CarController.class);
 
+    private final CarRouterService routerService;
+
+    @Autowired
+    public CarController(CarRouterService routerService) {
+        this.routerService = routerService;
+    }
+
     public CarResponse getCar(@RequestHeader("X-version") CarVersion version){
-
-
-
-        private final CarRouterService routerService;
-
-        return routerService.g(version,tokenAuthentication, orderUuid)
-                .doOnSuccess(response -> LOG.info("Resposta da simulação: [{}] com o orderUuid: [{}]", response, orderUuid))
-                .doOnError(error -> LOG.error("Ocorreu um erro ao efetuar a simulação com orderUuid: [{}] - Erro: [{}]", orderUuid, error));
+        return routerService.getCar(version);
     }
 
 }
