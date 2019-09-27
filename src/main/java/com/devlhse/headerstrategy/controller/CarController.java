b65a0a490383dd0a81a4a34db9564201a287f3b4
Luiz Evangelista <luiz.evangelista@b2wdigital.com>
2019-09-27 16:52:20 -0300

    corrige controller

--- src/main/java/com/devlhse/headerstrategy/controller/CarController.java
+++ src/main/java/com/devlhse/headerstrategy/controller/CarController.java
@@ -6,8 +6,12 @@ import com.devlhse.headerstrategy.service.CarRouterService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 public class CarController {
@@ -21,6 +25,8 @@ public class CarController {
         this.routerService = routerService;
     }
 
+    @ResponseStatus(value = OK)
+    @GetMapping("/cars")
     public CarResponse getCar(@RequestHeader("X-version") CarVersion version){
         return routerService.getCar(version);
     }
